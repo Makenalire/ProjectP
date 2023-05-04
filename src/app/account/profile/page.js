2c062d7263ea3fef809fe5bfaacb7a7c9abf54eb
SudLor <78199958+Makenalire@users.noreply.github.com>
2023-05-04 08:53:44 +0700

    Added profile page

--- src/app/account/profile/page.js
+++ src/app/account/profile/page.js
@@ -3,22 +3,47 @@
 import styles from "./profile.module.css";
 import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Profile() {
   const router = useRouter();
   const { data: session, status } = useSession();
-  
+  const [highScore, setHighScore] = useState("loading...");
 
   useEffect(() => {
     if (!session && status !== "loading") {
-        console.log("session = " + session + "status = " + status);
-        router.replace("../account");
+      console.log("session = " + session + "status = " + status);
+      router.replace("../account");
     }
   }, [session, status, router]);
 
+  const getHighScore = async () => {
+    let res = await fetch(
+      "http://localhost:3000/api/score?id=" + session.user.id
+    );
+    res = await res.json();
+    if (res) {
+      setHighScore(res.score);
+    } else {
+      setHighScore("N/A");
+    }
+  };
+
   if (session) {
-    return (<main className={styles.body}><div><p>Welcome, {session.user.name}</p><button className={styles.signout} onClick={signOut}>Sign Out</button></div></main>);
+    getHighScore();
+
+    return (
+      <main className={styles.body}>
+        <div>
+          <p>-[ {session.user.name} ]-</p>
+          <p>Email : {session.user.email}</p>
+          <p>Highest Score : {highScore}</p>
+          <button className={styles.signout} onClick={signOut}>
+            Sign Out
+          </button>
+        </div>
+      </main>
+    );
   }
 
   return <p>Loading...</p>;
